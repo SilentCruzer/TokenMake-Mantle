9d1721e0ac926b62af4530f4de067a7112ae2cc7
SilentCruzer <radithyakumar7@gmail.com>
2023-02-11 19:59:00 +0530

    Adds deploy scripts for NFT contract

--- hardhat-files/hardhat.config.js
+++ hardhat-files/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks : {
+    "mantle-testnet" : {
+      url: "https://rpc.testnet.mantle.xyz/",
+      accounts: [process.env.PRIV_KEY]
+    }
+  }
 };
